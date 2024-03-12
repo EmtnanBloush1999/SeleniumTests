ef16ddc51e82b45e63b48a5fa920f043f2a3d5b4
EmtnanBloush1999 <emtnan.bloush1999@gmail.com>
2024-03-12 10:03:13 +0300

    SoftAssertionsTests exercise

--- src/test/java/TestNG/GoogleTest.java
+++ src/test/java/TestNG/GoogleTest.java
@@ -2,7 +2,9 @@ package TestNG;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class GoogleTest extends TestBase{
 
@@ -15,14 +17,30 @@ public class GoogleTest extends TestBase{
      * 5. do a quick google search and assert that result stats is not empty using testng
      */
 
-    @Test
-    public void GoogleSearchTest(){
-
+    @Test(groups = {"sanityCheck", "guiTests"})
+    public void quickGoogleSearch(){
         bot.navigate("https://www.google.com/");
+        By searchInput = By.id("APjFqb");
+        bot.type(searchInput, "Selenium WebDriver" + Keys.RETURN);
+        By resultStatsLabel = By.id("result-stats");
+        Assert.assertNotEquals(driver.findElement(resultStatsLabel).getText(), "");
+    }
 
-        By SearchBar = By.id("APjFqb");
-        bot.type(SearchBar,"SDA"+ Keys.RETURN);
-        By result= By.id("result-stats");
-        assert !"".equals(driver.findElement(result).getText()):"it is empty";
+    @Test(testName = "Check Google Logo Exists", description = "Given I am on the Google homepage, Then the Google logo should be displayed")
+    public void assertGoogleLogoExists(){
+        bot.navigate("https://www.google.com/");
+        By googleLogoImage = By.cssSelector("img[alt='Google']");
+//        Assert.assertTrue(driver.findElement(googleLogoImage).isDisplayed());
+
+//        Assert.assertFalse(driver.findElement(googleLogoImage).isDisplayed(), "first check failed");
+//        Assert.assertTrue(driver.findElement(googleLogoImage).isDisplayed(), "second check failed");
+//        Assert.assertFalse(driver.findElement(googleLogoImage).isDisplayed(), "third check failed");
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertFalse(driver.findElement(googleLogoImage).isDisplayed(), "first check failed");
+        softAssert.assertTrue(driver.findElement(googleLogoImage).isDisplayed(), "second check failed");
+        softAssert.assertFalse(driver.findElement(googleLogoImage).isDisplayed(), "third check failed");
+        softAssert.assertAll();
+
     }
 }
